e4e0e081cb852f72ebe6e3c8127ae86f35bc0214
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-07 15:58:31 -0600

    dependencies added and reducer set up

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import thunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducer'
+
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  
-    <App />
-  ,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
