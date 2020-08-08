bc5f0c03c788955cb4a938447efe8f917d14b2f3
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-08 17:28:58 -0600

    characterList created

--- src/components/characters/CharacterList.js
+++ src/components/characters/CharacterList.js
@@ -1,16 +1,20 @@
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
+import { fetchCharacters } from '../../actions/users'
+
 const CharacterList = () => {
     const dispatch = useDispatch()
+    const characters = useSelector(state => state.userReducer.characters)
+    const currentUser = useSelector(state => state.authReducer.user_id)
 
     useEffect(() => {
-
+        dispatch(fetchCharacters(currentUser))
     }, [])
 
     return (
         <div>
-
+            {characters.map(character => <p>{character.name}</p>)}
         </div>
     )
 }
