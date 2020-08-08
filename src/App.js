3c0e0682fee9bf84d94184e750a68e429d225ca6
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-08 17:54:31 -0600

    added character list route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import SignIn from './components/signOn/SignIn'
+import CharacterList from './components/characters/CharacterList'
 
 function App() {
   return (
     <div>
-      <SignIn />
+      <Route exact path='/' component={SignIn} />
+      <Route path='/character-list' component={CharacterList} />
     </div>
   );
 }
