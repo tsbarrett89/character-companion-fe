8325e9b0e43be84949ea8b2b6597a20ffd95aef0
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-08 17:02:23 -0600

    user reducer set up

--- src/actions/users.js
+++ src/actions/users.js
@@ -4,7 +4,7 @@ export const FETCH_CHARACTERS_START = "FETCH_CHARACTERS_START"
 export const FETCH_CHARACTERS_SUCCESS = "FETCH_CHARACTERS_SUCCESS"
 export const FETCH_CHARACTERS_FAILURE = "FETCH_CHARACTERS_FAILURE"
 
-export const fetchCharacterList = user_id => dispatch => {
+export const fetchCharacters = user_id => dispatch => {
     dispatch({ type: FETCH_CHARACTERS_START })
     axios
         .get(`http://localhost:5000/characters/all/${user_id}`)
---
+++ src/reducer/userReducer.js
@@ -0,0 +1,36 @@
+import {
+    FETCH_CHARACTERS_START,
+    FETCH_CHARACTERS_SUCCESS,
+    FETCH_CHARACTERS_FAILURE
+} from '../actions/users'
+
+const initialState = {
+    characters: [],
+    isLoading: false,
+    error: null
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case FETCH_CHARACTERS_START:
+            return ({
+                ...state,
+                isLoading: true
+            })
+        case FETCH_CHARACTERS_SUCCESS:
+            return ({
+                ...state,
+                characters: action.payload,
+                isLoading: false
+            })
+        case FETCH_CHARACTERS_FAILURE:
+            return ({
+                ...state,
+                isLoading: false,
+                error: action.payload
+            })
+    }
+    return state
+}
+
+export default reducer
