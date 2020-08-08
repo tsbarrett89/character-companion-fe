2034707a31db822637812b6b3f10306aa18f3873
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-08 16:53:30 -0600

    action types and fetchCharacters created

---
+++ src/actions/users.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export const FETCH_CHARACTERS_START = "FETCH_CHARACTERS_START"
+export const FETCH_CHARACTERS_SUCCESS = "FETCH_CHARACTERS_SUCCESS"
+export const FETCH_CHARACTERS_FAILURE = "FETCH_CHARACTERS_FAILURE"
+
+export const fetchCharacterList = user_id => dispatch => {
+    dispatch({ type: FETCH_CHARACTERS_START })
+    axios
+        .get(`http://localhost:5000/characters/all/${user_id}`)
+        .then(res => {
+            console.log(res.data)
+            dispatch({ type: FETCH_CHARACTERS_SUCCESS, payload: res.data })
+        })
+        .catch(err => dispatch({ type: FETCH_CHARACTERS_FAILURE, payload: err.response }))
+}
---
+++ src/components/characters/CharacterList.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+const CharacterList = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CharacterList
