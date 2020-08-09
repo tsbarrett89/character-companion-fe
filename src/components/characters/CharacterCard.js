179684c5f4019958d229492edb160785ca738fd0
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-09 00:20:22 -0600

    mapping classes to display all

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -5,6 +5,7 @@ const CharacterCard = ({ character }) => {
     return (
         <div>
             <h2>{character.name}</h2>
+            {character.class.map(classType => <p key={classType.id}>{classType.class}</p>)}
             <h3>Level {character.level}</h3>
             <p>{character.race}</p>
             <p>{character.background}</p>
