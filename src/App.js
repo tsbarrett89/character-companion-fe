77e7df13882ba4a749e81374793ca1c11aaf3185
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-02 15:20:56 -0600

    Header added to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import Header from './components/Header'
 import SignIn from './components/signOn/SignIn'
 import CharacterList from './components/characters/CharacterList'
 
 function App() {
   return (
     <div>
+      <Header />
+      
       <Route exact path='/' component={SignIn} />
       <Route path='/character-list' component={CharacterList} />
     </div>
