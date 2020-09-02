956d5ee548ed54812069d6396e978bf38940d572
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-02 15:40:22 -0600

    username login condtional added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import SignOnButton from './signOn/SignOnButton'
 
 const Header = () => {
+    const username = useSelector(state => state.authReducer.username)
 
     return (
-        <>
-        </>
+        <header>
+            <h1>Character Companion</h1>
+            {username ? <h3>{username}</h3> : <SignOnButton />}
+        </header>
     )
 }
 
