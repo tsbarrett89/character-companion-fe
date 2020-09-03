325eeec83c1904dcc423d25f38b1f64973a221ff
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-03 10:44:16 -0600

    className signIn add for styling

--- src/components/authorization/SignIn.js
+++ src/components/authorization/SignIn.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 
+import './signIn.scss'
+
 import Login from './LoginForm'
 import Register from './RegisterForm'
 
@@ -7,7 +9,7 @@ const SignIn = () => {
     const [registered, setRegistered] = useState(true)
 
     return (
-        <div>
+        <div className="signIn">
             {registered ? <Login /> : <Register />}
             {registered ? <p onClick={() => setRegistered(!registered)}>Not yet a user? Register here</p> :
             <p onClick={() => setRegistered(!registered)}>Already registered? Login here</p>}
