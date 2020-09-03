47d81a4596ad4cbce22c65c4d8a61ae808d7d234
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-02 22:12:37 -0600

    using logOut action

--- src/components/authorization/LogOutButton.js
+++ src/components/authorization/LogOutButton.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { logOut } from '../../actions/auth'
 
 const LogOutButton = () => {
 
     return (
-        <NavLink to='/'>Log Out</NavLink>
+        <NavLink onClick={logOut} to='/'>Log Out</NavLink>
     )
 }
+
+export default LogOutButton
