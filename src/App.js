742ee4a0dc5b170214f72078afb4286cdf5c9a7a
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-03 10:43:36 -0600

    className app added for styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 
+import './app.scss'
+
 import Header from './components/Header'
 import SignIn from './components/authorization/SignIn'
 import CharacterList from './components/characters/CharacterList'
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       
       <Route exact path='/login' component={SignIn} />
