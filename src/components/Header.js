2594fa223a3dfe9f963864fb4577a1569f51d5c5
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-03 01:18:04 -0600

    header.scss imported

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
+
+import './header.scss'
 
 import SignOnButton from './authorization/SignOnButton'
 import LogOutButton from './authorization/LogOutButton'
