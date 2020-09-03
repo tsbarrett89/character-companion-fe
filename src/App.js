bd6a02eb784e21c38e33a392051733ba5f8b5461
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-02 21:58:28 -0600

    changed directory name to authorization

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 
 import Header from './components/Header'
-import SignIn from './components/signOn/SignIn'
+import SignIn from './components/authorization/SignIn'
 import CharacterList from './components/characters/CharacterList'
 
 function App() {
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
-import SignOnButton from './signOn/SignOnButton'
+import SignOnButton from './authorization/SignOnButton'
 
 const Header = () => {
     const username = useSelector(state => state.authReducer.username)
@@ -11,7 +11,7 @@ const Header = () => {
         <header>
             <h1>Character Companion</h1>
             {username && <h3>{username}</h3>}
-            {username ? <NavLink to='/login'>Log In / Register</NavLink> : <SignOnButton />}
+            {username ? <NavLink to='/'>Log Out</NavLink> : <SignOnButton />}
         </header>
     )
 }
