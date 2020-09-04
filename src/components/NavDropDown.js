10b4a21a6a7b37450b79f182392916a01c716b66
tsbarrett89 <tsbarrett89@gmail.com>
2020-09-04 15:47:31 -0600

    dropdown fixed, changed events to onClick

--- src/components/NavDropDown.js
+++ src/components/NavDropDown.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+
+import './NavDropDown.scss'
+import downWhite from '../images/down-arrow-white.png'
+
+import LogOutButton from './authorization/LogOutButton'
 
 const NavDropDown = () => {
+    const username = useSelector(state => state.authReducer.username)
+    const [hidden, setHidden] = useState(true)
 
     return (
-        <div>
-
+        <div className="navDropDown">
+            <span className="navDrop">
+                <h3>{username}</h3>
+                <img src={downWhite} alt="open nav drop down menu"
+                     onClick={() => setHidden(!hidden)}
+                />
+            </span>
+            {!hidden &&
+                <ul className="dropDown">
+                    <LogOutButton />
+                </ul>
+            }
         </div>
     )
 }
