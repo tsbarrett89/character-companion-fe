ec4a24699f9c3cb8dc9c4104eea924b563508440
tsbarrett89 <tsbarrett89@gmail.com>
2020-11-06 18:12:28 -0700

    characterCard now link to characterSheet

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CharacterCard = ({ character }) => {
 
     return (
-        <div>
+        <Link to={`/character-sheet/${character.name}`}>
             <h2>{character.name}</h2>
             {character.class.map(classType => <p key={classType.id}>{classType.class}</p>)}
             <h3>Level {character.level}</h3>
             <p>{character.race}</p>
             <p>{character.background}</p>
-        </div>
+        </Link>
     )
 }
 
